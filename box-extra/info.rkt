45d564fb24e6ddd9af77e845a7d1efe2586b8154
Bogdan Popa <bogdan@defn.io>
2022-12-11 18:16:27 +0200

    build: add license field to info.rkt files

--- box-extra/info.rkt
+++ box-extra/info.rkt
@@ -1,10 +1,10 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define version '("1.0"))
 (define collection 'multi)
 (define deps '("base"))
 (define build-deps '("racket-doc"
-
                      "box-extra-lib"
                      "rackunit-lib"
                      "scribble-lib"))
