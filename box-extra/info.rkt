041412cd5b122ecfb9cc2312f21d281e0d4b7143
Bogdan Popa <bogdan@defn.io>
2023-02-27 12:49:54 +0200

    build: fix version decl

--- box-extra/info.rkt
+++ box-extra/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version '("1.0"))
+(define version "1.0")
 (define collection 'multi)
 (define deps '("base"))
 (define build-deps '("racket-doc"
